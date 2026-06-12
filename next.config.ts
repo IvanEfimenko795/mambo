a5265ccf088b495d1ed58307b89f64f0d31edce5
IvanEfimenko795 <ivanefimenko795@gmail.com>
2026-06-12 15:42:36 +0300

    123

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,30 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const repoName = 'mambo';
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repo = "mambo";
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
+
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
 
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jolybell.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jolybell.com",
+        pathname: "/**",
+      },
+    ],
   },
-
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
