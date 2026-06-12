3b03efc53e1032e34765513760e0c97fd221adbb
IvanEfimenko795 <ivanefimenko795@gmail.com>
2026-06-12 15:47:30 +0300

    123

--- out/_next/static/2OTQ_1ILkqsCVGPM-VV3r/_buildManifest.js
+++ out/_next/static/2OTQ_1ILkqsCVGPM-VV3r/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ivanefimenko795-mambo//_next/:path+",
-        "destination": "/ivanefimenko795-mambo/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
